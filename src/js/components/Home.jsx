d57dc570fec490528862d3942266c062b89e6aa6
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-06-20 11:28:58 +0000

    updated list using bootstrap

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -31,26 +31,51 @@ const Home = () => {
 	
 	return (
 		<div className="text-center m-auto py-5">
+			<div className="d-flex justify-content-center align-items-center flex-column">
 			<h1>ToDo List</h1>
-            <input 
-				type="text" 
-				onChange={(e) => setTask(e.target.value)} 
-				value={task}
-			/>
-			<button onClick={(e) => addToList(e, task)}>Add to List</button>
+			<div className="d-flex mt-4 ">
+				<input 
+					type="text" 
+					className="btn btn-light border-dark"
+					onChange={(e) => setTask(e.target.value)} 
+					value={task}
+				/>
+				<button 
+					className="btn btn-dark" 
+					onClick={(e) => addToList(e, task)}>
+						Add to List
+				</button>
+			</div>
 
 			{/* display toDoList */}
-			<ul>
-				{toDoList.length === 0 ? (
-					<li>No tasks, add a task</li>
-				) : (
-					toDoList?.map((toDo, index) => {
-						if(toDo.done != true) {
-							return <li key={index}>{toDo.value} <span onClick={() => removeToDo(index)}>❌</span></li>
-						}							
-					})				
-				)}
-			</ul>	
+			
+				<div className="card mt-4" style={{width: "18rem"}}>
+					<div className="card-header">
+						My List
+					</div>
+					<div className="card-body text-center">
+						<ol className="list-group list-group-flush text-center">
+							{toDoList.length === 0 ? (
+								<li className="list-group-item">No tasks, add a task</li>
+							) : (
+								toDoList?.map((toDo, index) => {
+									if(toDo.done != true) {
+										return <li 
+											className="list-group-item" 
+											key={index}>{toDo.value} 
+												<span 
+													className="remover px-2" 
+													onClick={() => removeToDo(index)}>
+														❌
+												</span>
+										</li>
+									}							
+								})				
+							)}
+						</ol>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
